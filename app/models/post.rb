0f1d99b4c818770fb30c264e0d41061b236bae73
Aldair Pedro <greatestattackturtle@gmail.com>
2023-08-15 16:09:26 -0400

    add validations to post and user models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
 	has_many :comment
+	validates :Title, presence: true, length: { maximum: 6 }
+	validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 	has_many :post
 	has_many :comment
   validates :username, presence: true, length: { in: 5..20 }
-	validates :password, presence: true, length: { min: 6 }
+	validates :password, presence: true, length: { minimum: 6 }
 	validates :email, presence: true
 end
