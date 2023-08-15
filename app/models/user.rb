ae90c7f6b16512f7d4fd86ebd0a4771c6f6b8c0b
Aldair Pedro <greatestattackturtle@gmail.com>
2023-08-15 14:56:33 -0400

    add validations to user model and add associations to each model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	has_many :post
+	has_many :comment
+  validates :username, presence: true, length: { in: 5..20 }
+	validates :password, presence: true, length: { min: 6 }
+	validates :email, presence: true
 end
