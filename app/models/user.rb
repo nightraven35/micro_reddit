a9b39a09b77b482a3967823ddf3d38a1b7b92bbb
Aldair Pedro <greatestattackturtle@gmail.com>
2023-08-16 08:11:13 -0400

    pluralize has_many associations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :post
-  has_many :comment
+  has_many :posts
+  has_many :comments
   validates :username, presence: true, length: { in: 5..20 }
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, presence: true
