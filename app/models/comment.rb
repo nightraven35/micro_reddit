ae59146bab04319cb8b25eebb35cedb9ed5c8dda
Aldair Pedro <greatestattackturtle@gmail.com>
2023-08-16 08:15:21 -0400

    add validation to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true, length: { maximum: 200 }
+  validates :user_id, presence: true
+  validates :post_id, presence: true
 end
